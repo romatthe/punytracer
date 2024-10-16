063787120c4d5372c3338184578b3f8d8c48a415
Robin Mattheussen <me@romatthe.dev>
2024-10-16 08:14:24 +0200

    Create color alias

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,33 @@ use std::fs::File;
 use image::{ExtendedColorType, ImageEncoder};
 use image::codecs::png::PngEncoder;
 use indicatif::ProgressBar;
+use nalgebra_glm::Vec3;
 
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
+type Color = Vec3;
+
 fn main() -> Result<(), std::io::Error> {
-    let mut buffer = [0u8; IMAGE_WIDTH * IMAGE_HEIGHT * 3];
+    let mut buffer = vec![];
     let progress = ProgressBar::new((IMAGE_WIDTH * IMAGE_HEIGHT) as u64);
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f32) / (IMAGE_WIDTH - 1) as f32;
-            let g = (j as f32) / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.0;
+            let color = Color::new(
+                (i as f32) / (IMAGE_WIDTH - 1) as f32,
+                (j as f32) / (IMAGE_HEIGHT - 1) as f32,
+                0.0
+            );
 
-            let ir = (255.999 * r) as u8;
-            let ig = (255.999 * g) as u8;
-            let ib = (255.999 * b) as u8;
-
-            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3)] = ir;
-            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 1] = ig;
-            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 2] = ib;
-
+            buffer.push(color);
             progress.inc(1);
         }
     }
+
+    let buffer: Vec<u8> = buffer.iter()
+        .flat_map(to_rbg)
+        .collect();
 
     let file = File::create("traced.png").unwrap();
     let encoder = PngEncoder::new(file);
@@ -36,4 +38,12 @@ fn main() -> Result<(), std::io::Error> {
     progress.finish();
 
     Ok(())
+}
+
+fn to_rbg(color: &Color) -> [u8; 3] {
+    let r = (255.999 * color.x) as u8;
+    let g = (255.999 * color.y) as u8;
+    let b = (255.999 * color.z) as u8;
+
+    [r, g, b]
 }
