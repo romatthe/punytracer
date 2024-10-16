0ddab7a0a598d749e6a060f455d9487e63bdd8bd
Robin Mattheussen <me@romatthe.dev>
2024-10-16 07:46:34 +0200

    Save traced image as PNG

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::fs::File;
+use image::codecs::png::PngEncoder;
+use image::{ExtendedColorType, ImageEncoder};
+
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
 fn main() -> Result<(), std::io::Error> {
     let mut buffer = [0u8; IMAGE_WIDTH * IMAGE_HEIGHT * 3];
-
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
@@ -12,14 +14,19 @@ fn main() -> Result<(), std::io::Error> {
             let g = (j as f32) / (IMAGE_HEIGHT - 1) as f32;
             let b = 0.0;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
 
-            println!("{} {} {}", ir as u8, ig as u8, ib as u8);
+            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3)] = ir;
+            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 1] = ig;
+            buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 2] = ib;
         }
     }
 
-    // image::save_buffer("traced.png", &buffer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, image::ExtendedColorType::Rgb8).unwrap();
+    let file = File::create("traced.png").unwrap();
+    let encoder = PngEncoder::new(file);
+    encoder.write_image(&buffer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, ExtendedColorType::Rgb8).unwrap();
+
     Ok(())
 }
