edd653b3cba753a8b6ba8e0a51acbc09f4ecd127
Robin Mattheussen <me@romatthe.dev>
2024-10-16 08:01:46 +0200

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
-use image::codecs::png::PngEncoder;
+
 use image::{ExtendedColorType, ImageEncoder};
+use image::codecs::png::PngEncoder;
+use indicatif::ProgressBar;
 
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
 fn main() -> Result<(), std::io::Error> {
     let mut buffer = [0u8; IMAGE_WIDTH * IMAGE_HEIGHT * 3];
+    let progress = ProgressBar::new((IMAGE_WIDTH * IMAGE_HEIGHT) as u64);
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
@@ -21,12 +24,16 @@ fn main() -> Result<(), std::io::Error> {
             buffer[(IMAGE_HEIGHT * j * 3) + (i * 3)] = ir;
             buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 1] = ig;
             buffer[(IMAGE_HEIGHT * j * 3) + (i * 3) + 2] = ib;
+
+            progress.inc(1);
         }
     }
 
     let file = File::create("traced.png").unwrap();
     let encoder = PngEncoder::new(file);
     encoder.write_image(&buffer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, ExtendedColorType::Rgb8).unwrap();
+
+    progress.finish();
 
     Ok(())
 }
